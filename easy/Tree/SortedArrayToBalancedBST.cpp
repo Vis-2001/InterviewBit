57c0ee74f06ac64f7375c5b87bf8b916b2cbbd65
Vishal J Lodha <unuiwnthing@gmail.com>
2024-02-08 23:03:41 +0530

    Sorted Array to Balanced Binary Tree

---
+++ easy/Tree/SortedArrayToBalancedBST.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x): val(x),left(NULL),right(NULL){}
+};
+TreeNode* rec(const vector<int>A,int s,int e){
+    if(s>e)return NULL;
+    int m=s+(e-s)/2;
+    TreeNode* ans=new TreeNode(A[m]);
+    ans->left=rec(A,s,m-1);
+    ans->right=rec(A,m+1,e);
+    return ans;
+}
+TreeNode* Solution::sortedArrayToBST(const vector<int> &A) {
+    int s=A.size()-1;
+    return rec(A,0,s);
+}
