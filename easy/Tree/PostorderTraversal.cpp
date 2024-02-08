b59b119430db765bc01c6bafc67ceb3e031792da
Vishal J Lodha <unuiwnthing@gmail.com>
2024-02-08 23:11:06 +0530

    Post order traversal

---
+++ easy/Tree/PostorderTraversal.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x): val(x),left(NULL),right(NULL){}
+};
+ void postorder(vector<int>&ans,TreeNode* A){
+     if(A==NULL)return;
+     postorder(ans,A->left);
+     postorder(ans,A->right);
+     ans.push_back(A->val);
+ }
+vector<int> Solution::postorderTraversal(TreeNode* A) {
+    vector<int>ans;
+    postorder(ans,A);
+    return ans;
+}
